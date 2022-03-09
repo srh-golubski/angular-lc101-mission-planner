fbc57f41dddcaab47c2b81b377dff3511f9dbc82
srh-golubski <srh.sexton@gmail.com>
2022-03-08 20:49:56 -0600

    Angular studio 2

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,3 +1,4 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipmentItems: object[] = [
+    {name: 'Duct Tape', mass: 0.5},
+    {name: 'Space Camera', mass: 20},
+    {name: 'Food', mass: 150},
+    {name: 'Oxygen Tanks', mass: 400},
+    {name: 'AE-35 Unit', mass: 5},
+    {name: 'ISS Supplies', mass: 800},
+    {name: 'Water', mass: 250},
+    {name: 'Satellite', mass: 1200},
+    {name: 'R2 Unit', mass: 32}
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
   constructor() { }
 
   ngOnInit() {
   }
 
+  addItem(name: string, mass: number) {
+    this.cargoHold.push({name: name, mass: mass});
+    this.cargoMass += item['mass'];
+  }
 }
