d735df30d2c5c63e99d19d655f7cccab8995a1f5
srh-golubski <srh.sexton@gmail.com>
2022-03-10 21:53:00 -0600

    stopping point

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,3 +1,4 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,26 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() { }
+  equipmentItems: object[] = [
+    {name: 'Duct Tape', mass: 0.5},
+    {name: 'Space Camera', mass: 20},
+    {name: 'Food', mass: 150},
+    {name: 'Oxygen Tanks', mass: 400},
+    {name: 'AE-35 Unit', mass: 5},
+    {name: 'ISS Supplies', mass: 800},
+    {name: 'Water', mass: 250},
+    {name: 'Satellite', mass: 1200},
+    {name: 'R2 Unit', mass: 32}
+  ];
 
-   // Code your addItem function here:
-   
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
+  itemAdd: boolean = true
+
+  ngOnInit() {
+  }
+
+  addItem(name: string, mass: number) {
+    this.cargoHold.push({name: name, mass: mass});
+    this.cargoMass += mass;
+    if (this.cargoMass + 200 <= this.maximumAllowedMass) {
+      this.itemAdd = true;
+    } else {
+      this.itemAdd = false;
+    }
+  }
+
+  emptyCargo() {
+    this.cargoHold = [];
+    this.cargoMass = 0;
+    this.itemAdd = true;
+  }
 }
